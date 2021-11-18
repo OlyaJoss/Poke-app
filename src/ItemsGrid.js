8cdeb9009f21c81394d2641caffbd646b2287c8c
OlyaJoss <65188310+OlyaJoss@users.noreply.github.com>
2021-11-18 16:08:11 +0300

    map fix commit

--- src/ItemsGrid.js
+++ src/ItemsGrid.js
@@ -7,13 +7,15 @@ export default class ItemsGrid extends Component {
     render() {
         console.log(this.props.itemsData)
         return (
-            (this.props.itemsData && this.props.itemsData.map(({ name }) =>
-                <div className='grid-item-wrapper'>
-                    <div className='item-wrapper'>
-                        <img></img>
-                        <p>{name}</p>
-                    </div>
+            <div className='grid-item-wrapper'>
+            {(this.props.itemsData && this.props.itemsData.map(({ name }) =>
+            
+                <div className='item-wrapper'>
+                    <img></img>
+                    <p>{name}</p>
                 </div>
-            )))
+           
+        ))}
+             </div>)
     }
 }
